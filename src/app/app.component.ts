e85365c7420b6d352c43a5bd14e15aa0536c21c0
Tarmizi Hamid <tarmizi2001@gmail.com>
2020-09-22 21:03:58 +0700

    section 3 all changes (components)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
     selector: 'mw-app',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-    constructor() { }
+export class AppComponent{
+    firstMediaItem = {
+        id: 1,
+        name: 'Firebug',
+        category: 'Science Fiction',
+        year: '2000',
+        watchedOn: 1294166565384,
+        isFavorite: false
+    }
 
-    ngOnInit(): void { }
+    onMediaItemDelete(mediaItem){}
 }
