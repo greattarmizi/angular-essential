9fc9fee6270ac092eb9640632645bae43b6f8b05
Tarmizi Hamid <tarmizi2001@gmail.com>
2020-11-21 22:30:13 +0700

    This is template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
     imports: [ 
-        BrowserModule 
+        BrowserModule,
+        FormsModule 
     ],
     declarations: [
         AppComponent,
         MediaItemComponent,
         MediaItemListComponent,
-        FavoriteDirective
+        FavoriteDirective,
+        MediaItemFormComponent
     ],
     bootstrap: [
         AppComponent
